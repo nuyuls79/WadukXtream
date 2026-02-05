d8d9e954405d5a9684429ada92693a16ed006878
michat88 <gokilmichat@gmail.com>
2026-02-06 06:55:37 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
+    // Plugin standar Aplikasi
     alias(libs.plugins.android.application) apply false
+
+    // [PENTING] Baris ini dikembalikan dari 'rootlama' agar modul ':library' terbaca
     alias(libs.plugins.android.library) apply false
+
+    // Plugin baru dari 'rootbaru'
+    alias(libs.plugins.android.lint) apply false
+    alias(libs.plugins.android.multiplatform.library) apply false
+    
+    // Plugin pendukung lainnya (Sama di kedua versi)
     alias(libs.plugins.buildkonfig) apply false // Universal build config
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -9,6 +18,7 @@ plugins {
 }
 
 allprojects {
+    // Konfigurasi tasks dari 'rootbaru' untuk kompatibilitas Gradle masa depan
     // https://docs.gradle.org/current/userguide/upgrading_major_version_9.html#test_task_fails_when_no_tests_are_discovered
     tasks.withType<AbstractTestTask>().configureEach {
         failOnNoDiscoveredTests = false
